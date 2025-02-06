caf441f29ff63c27231fc880d21a8c0aa8fb5b08
JimmyTea1026 <59228524+JimmyTea1026@users.noreply.github.com>
2025-02-06 16:32:48 +0800

    實作 load_model_local

--- YoloModel.py
+++ YoloModel.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import torch
 import cv2
+import os
 from ultralytics import YOLO
 from BaseModel import BaseModel
 from Normalizer import Normalizer
@@ -9,28 +10,31 @@ from Normalizer import Normalizer
 class YOLOModel(BaseModel):      
     def __init__(self, height: int = 640, width: int = 640, conf_threshold: float = 0.2, normalizer=None):  
         super().__init__()
-        self.__model_height = height
-        self.__model_width = width
+        self._model_height = height
+        self._model_width = width
         self.__conf_threshold = conf_threshold
         self._normalizer = normalizer
+    
+    def _load_model_local(self, model_path):  
+        """  
+        從本地載入 YOLO 模型  
+        
+        Args:  
+            model_path (str): 模型文件路徑  
+        """  
+        if not os.path.exists(model_path):  
+            raise FileNotFoundError(f"模型文件不存在：{model_path}")  
+        self._model = YOLO(model_path)  
     
     def _load_model_online(self):
         # 從網路載入預訓練模型，載入yolov10模型
         self._model = YOLO("yolov10n.pt")
         return 
     
-    def preprocess(self, image: np.ndarray) -> torch.Tensor:  
-        # 實作 YOLO 的預處理邏輯  
-        image = cv2.resize(image, (self.__model_width, self.__model_height))
-        tensor = torch.from_numpy(image).float()  
-        tensor = tensor.permute(2, 0, 1)  # HWC to CHW  
-        tensor = tensor.unsqueeze(0)  # 增加 batch 維度  
-        tensor = tensor.to(self.device)  
-        return tensor
-    
+    @torch.no_grad() 
     def inference(self, input_data):
         results = self._model.predict(source=input_data, 
-                                      imgsz=(self.__model_width, self.__model_height), 
+                                      imgsz=(self._model_width, self._model_height), 
                                       conf=self.__conf_threshold)
         return results
 
@@ -49,19 +53,18 @@ def main():
         model = YOLOModel(**config)  
         
         # 載入模型  
-        model.load_model()  
+        model.load_model("./assets/yolov10n.pt")  
         
         # 讀取影片
-        cap = cv2.VideoCapture("demo.mp4")
+        cap = cv2.VideoCapture("./assets/demo.mp4")
         while True:
             ret, frame = cap.read()
             if not ret:
                 break
             image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            results = model.inference(image)
+            results = model(image)
             annotated_image = model.plot(results)
             annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR)
-            annotated_image = cv2.resize(annotated_image, (640, 384))
             cv2.imshow("Annotated Image", annotated_image)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
